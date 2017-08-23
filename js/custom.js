87c367a50811191f4e89942942afa2d4e0ab3e2c
Dutta3 <Abhishek_Dutta3@Dell.com>
2017-08-23 10:26:26 +0530

    First commit

---
+++ js/custom.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+   $('.parallax').parallax();
+   $('.collapsible').collapsible();
+   $('.scrollspy').scrollSpy();
+   $('.tooltipped').tooltip({delay: 50});
+});
+
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
