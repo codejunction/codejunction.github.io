c2746881746cc5c3cd1219b6200b6326f76519e7
Dutta3 <Abhishek_Dutta3@Dell.com>
2018-03-30 17:11:30 +0530

    aos fix

--- js/script.js
+++ js/script.js
@@ -5,7 +5,15 @@ $(document).ready(function(){
    $('.scrollspy').scrollSpy();
    $('.tooltipped').tooltip({delay: 50});
 });
+//--------------------------Analytics-------------------------------------------
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
+ga('create', 'UA-105150607-1', 'auto');
+ga('send', 'pageview');
+//---------------------------SideNav--------------------------------------------
 function openNav() {
     document.getElementById("myNav").style.width = "100%";
 }
